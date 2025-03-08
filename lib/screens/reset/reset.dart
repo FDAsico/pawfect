61c1f5d38a20ebf7ff8342c095b1a30fa4d21349
FDAsico <fasico@gbox.adnu.edu.ph>
2025-03-09 02:58:15 +0800

    PAW-15 Reset Password Screen

---
+++ lib/screens/reset/reset.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+/* 
+Authored by: Francis Dave A. Asico
+Company: JaCode Facctory
+Project: Pawfect Naga
+Feature: [PAW-15] Reset Password Screen
+Description: Reset Password Screen for those users who click the link in their email
+*/
+
+class Reset extends StatelessWidget {
+  const Reset({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/assets/images/Lilly 2.png'
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+              child: Text(
+                'Reset Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 36,
+                  color: Color.fromARGB(255, 91, 72, 139),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 30.0),
+              child: Text(
+                "Set your new password",
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 13,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+              child: Text(
+                'Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+              child: SizedBox(
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your new password',
+                  ),
+                ),
+              )
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.fromLTRB(20.0, 35.0, 20.0, 0.0),
+              child: Text(
+                'Confirm Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+              child: SizedBox(
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Confirm Password',
+                  ),
+                ),
+              )
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20.0, 45.0, 20.0, 25.0),
+              width: 351.0,
+              height: 58.0,
+              child: SizedBox(
+                child: FilledButton(
+                  onPressed: () => debugPrint('Reset Button Pressed!'), 
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      )
+                    )
+                  ),
+                  child: Text(
+                    'Reset',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              )
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
