89ec9fd1bbef804b41145d93d5cc4d805d73ea24
FDAsico <fasico@gbox.adnu.edu.ph>
2025-03-09 02:59:08 +0800

    PAW-6 Signup Screen

--- lib/screens/signup/signup.dart
+++ lib/screens/signup/signup.dart
@@ -15,177 +15,181 @@ class SignUp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: ListView(
           children: [
-            Container(
-              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-              child: Text(
-                'PAWFECT NAGA',
-                style: TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 36,
-                  color: Color.fromARGB(255, 91, 72, 139),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 25.0),
-              child: Text(
-                'Sign Up',
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 30.0),
-              child: Text(
-                "Create a new account to join our buddy community",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-              child: Text(
-                'Name',
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
-              child: SizedBox(
-                child: TextField(
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter your name',
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                child: Text(
+                  'PAWFECT NAGA',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 36,
+                    color: Color.fromARGB(255, 91, 72, 139),
                   ),
                 ),
               ),
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-              child: Text(
-                'Email',
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
-              child: SizedBox(
-                child: TextField(
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter your email',
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 25.0),
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 24,
                   ),
                 ),
               ),
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-              child: Text(
-                'Password',
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
-              child: SizedBox(
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter your password',
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 30.0),
+                child: Text(
+                  "Create a new account to join our buddy community",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20,
                   ),
                 ),
-              )
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-              child: Text(
-                'Confirm Password',
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 20,
-                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
-              child: SizedBox(
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Confirm Password',
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                child: Text(
+                  'Name',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20,
                   ),
                 ),
-              )
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 25.0),
-              width: 351.0,
-              height: 58.0,
-              child: SizedBox(
-                child: FilledButton(
-                  onPressed: () => debugPrint('Sign Up Button Pressed!'), 
-                  style: ButtonStyle(
-                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+                child: SizedBox(
+                  child: TextField(
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your name',
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                child: Text(
+                  'Email',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+                child: SizedBox(
+                  child: TextField(
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your email',
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                child: Text(
+                  'Password',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+                child: SizedBox(
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                )
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                child: Text(
+                  'Confirm Password',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+                child: SizedBox(
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Confirm Password',
+                    ),
+                  ),
+                )
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 25.0),
+                width: 351.0,
+                height: 58.0,
+                child: SizedBox(
+                  child: FilledButton(
+                    onPressed: () => debugPrint('Sign Up Button Pressed!'), 
+                    style: ButtonStyle(
+                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        )
                       )
-                    )
+                    ),
+                    child: Text(
+                      'Sign Up',
+                    ),
                   ),
-                  child: Text(
-                    'Sign Up',
+                )
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: 110,
+                    child: Divider(),
                   ),
-                ),
-              )
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  width: 110,
-                  child: Divider(),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  width: 130,
-                  child: Text('or sign up with'),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  width: 110,
-                  child: Divider(),
-                ),
-              ],
-            ),
-          ],
-        ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: 130,
+                    child: Text('or sign up with'),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: 110,
+                    child: Divider(),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+        ],
       )
     );
   }
