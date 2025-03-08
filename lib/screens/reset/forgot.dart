d5df7fe2242c19a6e15dcde1fd9f7b7e356b6361
FDAsico <fasico@gbox.adnu.edu.ph>
2025-03-09 02:56:54 +0800

    PAW-14 Forgot Password Screen

---
+++ lib/screens/reset/forgot.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+/* 
+Authored by: Francis Dave A. Asico
+Company: JaCode Facctory
+Project: Pawfect Naga
+Feature: [PAW-14] Forgot Password Screen
+Description: For users who wants to recover their account if they forgot their password
+*/
+
+class Forgot extends StatelessWidget {
+  const Forgot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+        body: ListView(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'lib/assets/images/Lilly 2.png'
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                  child: Text(
+                    'Forgot Password',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 36,
+                      color: Color.fromARGB(255, 91, 72, 139),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 30.0),
+                  child: Text(
+                    "Let's Recover your account!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                  child: Text(
+                    'Email',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+                  child: SizedBox(
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter your Email',
+                      ),
+                    ),
+                  )
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+                  width: 351.0,
+                  height: 58.0,
+                  child: SizedBox(
+                    child: FilledButton(
+                      onPressed: () => debugPrint('Reset Button Pressed!'), 
+                      style: ButtonStyle(
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          )
+                        )
+                      ),
+                      child: Text(
+                        'Reset',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  )
+                ),
+              ],
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
