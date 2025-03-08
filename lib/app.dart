76f4a1cb0d144f8c96723c8172c5ec1524b41a74
FDAsico <fasico@gbox.adnu.edu.ph>
2025-03-08 16:37:33 +0800

    Added ThemeData

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pawfect/screens/login/login.dart';
+import 'package:google_fonts/google_fonts.dart';
 // import 'screens/home/home.dart';
 
 class App extends StatelessWidget {
@@ -8,7 +9,28 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Pawfect Naga',
       home: Login(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: Color.fromARGB(255, 95, 79, 134),
+          //onPrimary: onPrimary,
+          //secondary: secondary,
+          //onSecondary: onSecondary,
+        ),
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: GoogleFonts.poppins(
+            fontSize: 30,
+            fontStyle: FontStyle.italic,
+          ),
+          bodyMedium: GoogleFonts.poppins(),
+          displaySmall: GoogleFonts.poppins(),
+        ),
+      ),
     );
   }
 }
