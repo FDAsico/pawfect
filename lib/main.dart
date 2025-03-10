69f8a493afb5c1f7552e4041243291958e9194ed
FDAsico <fasico@gbox.adnu.edu.ph>
2025-03-10 19:41:37 +0800

    Implement SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'app.dart';
 
-void main() => runApp(App());
+int initScreen = 0;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final initScreen = prefs.getInt('initScreen');
+  await prefs.setInt("initScreen", 1);
+  debugPrint('initScreen $initScreen');
+  runApp(App());
+}
