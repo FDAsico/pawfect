24fe5b7db840f327c366a65ac439975e391eb3e1
FDAsico <fasico@gbox.adnu.edu.ph>
2025-03-11 00:21:51 +0800

    PAW-4 Added delay to Splash Screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:pawfect/main.dart';
 import 'package:pawfect/screens/login/login.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pawfect/screens/onboarding/onbording.dart';
 // import 'screens/home/home.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState(){
+    super.initState();
+    initialization();
+  }
+  void initialization() async {
+    debugPrint('Pausing...');
+    await Future.delayed(const Duration(seconds: 3));
+    debugPrint('Unpausing');
+    FlutterNativeSplash.remove();
+  } 
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'app.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 int? initScreen;
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   SharedPreferences prefs = await SharedPreferences.getInstance();
   initScreen = prefs.getInt('initScreen');
   await prefs.setInt("initScreen", 1);
