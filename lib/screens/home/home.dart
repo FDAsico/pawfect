2113cd5e75bcc021b9557f0c4a9596ae5c5f30fb
FDAsico <fasico@gbox.adnu.edu.ph>
2025-03-11 22:05:19 +0800

    PAW-7 Added Content to the Home Screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 
-// class Home extends StatelessWidget {
-//   const Home({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Hello'),
-//       ),
-//       body: Text(''),
-//     );
-//   }
-// }
+/* 
+Authored by: Francis Dave A. Asico
+Company: JaCode Facctory
+Project: Pawfect Naga
+Feature: [PAW-7] Home Screen
+Description: Home screen for users who are able to login
+*/
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -22,8 +16,89 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int currentPageIndex = 0;
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var brightness = MediaQuery.of(context).platformBrightness;
+    bool isDarkMode = (brightness == Brightness.dark);
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              onPressed: () { Scaffold.of(context).openDrawer(); },
+              icon: const Icon(Icons.menu),
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          }
+        ),
+        title: currentPageIndex == 0 ? Text(
+          'Hi Francis, How are you doing today?',
+            style: TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.normal,
+            ),
+          ) : null, 
+        actionsPadding: EdgeInsets.only(right: 15.0),
+        actions: [
+          IconButton(
+            onPressed: () => (), 
+            icon: Icon(Icons.notifications_outlined),
+            splashRadius: 8,
+            style: ButtonStyle(
+              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              )
+            ),
+          ),
+          IconButton(
+            onPressed: () => (), 
+            icon: Icon(Icons.shopping_cart_outlined),
+            splashRadius: 8,
+            style: ButtonStyle(
+              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              )
+            ),
+          )
+        ],
+      ),
+      drawer: Drawer(),
+      body: PageView(
+
+      ),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: isDarkMode? Color(0xFF4A4459) : Color(0xFFD1BDF2),
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_outlined)),
+            selectedIcon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(label: Text('2'), child: Icon(Icons.messenger_outline_outlined)),
+            selectedIcon: Badge(label: Text('2'), child: Icon(Icons.messenger_sharp)),
+            label: 'Chats',
+          ),
+        ],
+      ),
+    );
   }
 }
